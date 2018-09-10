d30b7270f71791ac1410dfed6c4713fa4dbaa935
coinbitbot <35292111+coinbitbot@users.noreply.github.com>
2018-09-10 12:41:55 +0300

    Add main algorithm for stocks

--- Bot1.java
+++ Bot1.java
@@ -51,11 +51,6 @@ public class Bot1 {
         }
     }
 
-    private String encodePair(String pair) {
-        return pair.replace("-", "_").toLowerCase();
-    }
-
-
     public double getBTCAmount(String publicKey, String privateKey) {
         return 0;
     }
@@ -66,48 +61,6 @@ public class Bot1 {
 
     public Object placeSellOrder(String publicKey, String privateKey, String pair, double amount, double price) throws InvalidApiException, SymbolNotExistsException, InterruptedException {
         return buy$sell(publicKey, privateKey, pair, amount, price, false);
-    }
-
-    private Object buy$sell(String publicKey, String privateKey, String pair, double amount, double price, boolean type) throws InvalidApiException, SymbolNotExistsException, InterruptedException {
-        String answerStr = "";
-        try {
-
-            DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.ENGLISH);
-            otherSymbols.setGroupingSeparator('.');
-            DecimalFormat format = new DecimalFormat("##.########", otherSymbols);
-
-            List<NameValuePair> getParams = new ArrayList<>();
-            getParams.add(new BasicNameValuePair("method", "Trade"));
-            getParams.add(new BasicNameValuePair("nonce", String.valueOf(System.currentTimeMillis() / 1000)));
-            getParams.addAll(createParamsForTrade(type, pair, format.format(amount), format.format(price)));
-            answerStr = makePrivateRequest(TIDEX_URL, RequestMethod.POST, getParams, publicKey, privateKey);
-            //System.out.println(" ===== "  + answerStr);
-            JSONObject answer = new JSONObject(answerStr);
-            JSONObject json = answer.getJSONObject("return");
-            if (answer.keySet().size() == 0) return null;
-            String order_id = json.getLong("order_id") + "";
-            double amount_order = Double.parseDouble(format.format(amount));
-            //---------------------------------
-            if (type) amounts.put(order_id, amount_order);
-            //---------------------------------));
-            return new OrderEntity(order_id, getTimestamp(new Date().getTime()), pair, amount_order, Double.parseDouble(format.format(price)), "1");
-        } catch (Exception e) {
-            System.out.println("Pizdec---------------------------------------------------");
-            String byu$sell = type ? "buy" : "sell";
-            Logger.logException("Exception while placing " + byu$sell + " order. Received responce = " + answerStr, e, false);
-            Thread.sleep(Resources.DELAY);
-            return buy$sell(publicKey, privateKey, pair, amount, price, type);
-        }
-    }
-
-    private List<NameValuePair> createParamsForTrade(boolean b, String pair, String amount, String price) {
-        List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("pair", encodePair(pair)));
-        params.add(new BasicNameValuePair("type", b ? "buy" : "sell"));
-        params.add(new BasicNameValuePair("rate", price));
-        params.add(new BasicNameValuePair("amount", amount));
-        if (b) VolumeService.addVolume(Stocks.Tidex, pair, Double.parseDouble(amount));
-        return params;
     }
 
     @Override
@@ -213,16 +166,6 @@ public class Bot1 {
         }
     }
 
-    private double getAmountById(String order_id) {
-        double amount = 0;
-        if (amounts.containsKey(order_id)) {
-            amount = amounts.get(order_id);
-            amounts.remove(order_id);
-        }
-        return amount;
-    }
-
-
     @Override
     public double getAmountByCurrency(String publicKey, String privateKey, String pair, boolean currency) throws InterruptedException, SymbolNotExistsException {
         String answer = "";
@@ -242,13 +185,6 @@ public class Bot1 {
         }
         return amount;
     }
-
-    private String getCurrency(String pair, boolean currency) {
-        if (currency) return pair.substring(0, pair.indexOf('-')).toLowerCase();
-        else return pair.substring(pair.indexOf('-') + 1).toLowerCase();
-
-    }
-
 
     @Override
     public List<OrderEntity> getActiveOrders(String publicKey, String privateKey, String pair) throws InterruptedException {
@@ -277,88 +213,5 @@ public class Bot1 {
             Thread.sleep(Resources.DELAY);
             return getActiveOrders(publicKey, privateKey, pair);
         }
-    }
-
-    private Timestamp getTimestamp(Long str) {
-        try {
-            return new Timestamp(str);
-        } catch (Exception e) { //this generic but you can control another types of exception
-            Logger.logException("While parsing timestamp: ", e, false);
-            return null;
-        }
-    }
-
-
-    private String makePrivateRequest(String url, RequestMethod method, List<NameValuePair> params, String publicKey, String privateKey) {
-        String answer = "";
-        try {
-            StringBuilder sb = new StringBuilder();
-            for (NameValuePair postParam : params) {
-                if (sb.length() > 0) {
-                    sb.append("&");
-                }
-                sb.append(postParam.getName()).append("=").append(postParam.getValue());
-            }
-
-            if (method.equals(RequestMethod.GET))
-                answer = RequestsHelper.getHttp(url + params.get(0).getValue(), null);
-            if (method.equals(RequestMethod.POST)) {
-                String body = sb.toString();
-                Mac mac = Mac.getInstance("HmacSHA512");
-                mac.init(new SecretKeySpec(privateKey.getBytes(), "HmacSHA512"));
-                String signature = new String(Hex.encodeHex(mac.doFinal(body.getBytes())));
-                List<NameValuePair> httpHeaders = new ArrayList<>();
-                httpHeaders.add(new BasicNameValuePair("Key", publicKey));
-                httpHeaders.add(new BasicNameValuePair("Sign", signature));
-                answer = RequestsHelper.postHttp(url, params, httpHeaders);
-            }
-
-            JSONObject json = new JSONObject(answer);
-            if (json.has("success") && json.getInt("success") == 0) {
-                String tradePair = getPairFromParams(params);
-                reportAnError(json, tradePair);
-            }
-
-            return answer;
-        } catch (Exception e) {
-            Logger.logException("While sending private request", e, true);
-        }
-        return "Blyooooo";
-    }
-
-    private String getError(JSONObject answer) {
-
-        // for post method`
-        if (answer.has("code")) {
-            int code = answer.getInt("code");
-            if (code == 804 || code == 807 || code == 831 || code == 0 || code ==832)
-                return answer.getString("error");
-        }
-
-        // for some exceptiom in get method (in future)
-        String error = answer.getString("error");
-        if (error.equals(""))
-            return answer.getString("error");
-
-        return null;
-
-    }
-
-    private String getPairFromParams(List<NameValuePair> params) {
-        List<NameValuePair> validParams = params.stream().filter(e -> e.getName().equals("pair")).collect(Collectors.toList());
-        String tradePair = "";
-        if (!validParams.isEmpty())
-            tradePair = validParams.get(0).getValue();
-        return tradePair;
-    }
-
-
-    private void reportAnError(JSONObject json, String tradePair) {
-        System.out.println("Answer: " + json + ". " + new Date());
-        String error = getError(json);
-        if (error != null)
-            SenderMessage.sendMessage(Resources.ALERT_BOT_TOKEN, Resources.ALERT_CHAT_ID,
-                    Logger.messageToAdmin(Stocks.Tidex, json.toString(), tradePair));
-
     }
 }
