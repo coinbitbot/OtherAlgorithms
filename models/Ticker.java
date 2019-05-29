8dbd1a0a5228c81e2dbf322a60e3dbdcfb937b98
coinbitbot <coinbitbot@gmail.com>
2019-05-29 22:41:09 +0300

    Ticker model, preparing new bot

--- models/Ticker.java
+++ models/Ticker.java
@@ -1,4 +1,15 @@
 package models;
 
+@Data
+@ToString
+@AllArgsConstructor
 public class Ticker {
+    private double high;
+    private double low;
+    private double bid;
+    private double ask;
+    private double last;
+    private double volume;
+    private double baseVolume;
+    private long timestamp;
 }
